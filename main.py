e95724fb2bb7a6aede9830be0a757282b22f42e8
Suvangi-Chakraborty <suvangi.chakraborty2000@gmail.com>
2023-05-12 20:14:07 +0530

    Pie Chart displayed inside webpage

--- main.py
+++ main.py
@@ -4,10 +4,14 @@ from flask import Flask, request
 from sumy.summarizers.lex_rank import LexRankSummarizer
 from sumy.nlp.tokenizers import Tokenizer
 from sumy.parsers.plaintext import PlaintextParser
+import matplotlib
+matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
 from bs4 import BeautifulSoup
 import nltk
+import base64
+import re
 
 nltk.download('punkt')
 nltk.download('vader_lexicon')
@@ -28,8 +32,6 @@ def index():
 def process_noun():
     # Fetching news
     results = []
-    sentiment_counts = [0, 0, 0]
-    overall_polarity_score = 0
 
     search_word = request.form['noun']
     no_of_articles = 10
@@ -39,8 +41,6 @@ def process_noun():
     response = requests.get(url)
     data = response.json()
     articles = data.get('articles')
-
-    sentiment_counts=[0,0,0]
 
     for article in articles:
         title = article['title']
@@ -71,70 +71,63 @@ def process_noun():
         else:
             sentiment = 'Neutral'
 
-        # Update sentiment counts and overall polarity score
-        if sentiment == "Positive":
-            sentiment_counts[0] += 1
-        elif sentiment == "Negative":
-            sentiment_counts[1] += 1
-        else:
-            sentiment_counts[2] += 1
-
-        overall_polarity_score += sentiment_scores['compound']
-
         # Store the results
         result = {
             "title": title,
             "url": url,
             "sentiment": sentiment,
-            "positive_count": sentiment_counts[0],
-            "negative_count": sentiment_counts[1],
-            "neutral_count": sentiment_counts[2],
             "overall_polarity_score": sentiment_scores['compound']
         }
         results.append(result)
 
-    # Calculate average sentiment counts and overall polarity score
-    average_sentiment_counts = [count / len(articles) for count in sentiment_counts]
-    average_polarity_score = overall_polarity_score / len(articles)
-
-
-    # Calculate average sentiment counts and overall polarity score
-    average_sentiment_counts = [count / len(articles) for count in sentiment_counts]
-    average_polarity_score = overall_polarity_score / len(articles)
-
-    sentiments = ["Positive", "Negative", "Neutral"]
-    colors = ['#00ff00', '#ff0000', '#ffff00']
-
-    plt.bar(sentiments, average_sentiment_counts, color=colors)
-    plt.title("Average Sentiment Analysis")
-    plt.xlabel("Sentiment")
-    plt.ylabel("Count")
-    plt.show()
-
-    # Create the pie chart using average sentiment counts
-    labels = ['Positive', 'Negative', 'Neutral']
-    sizes = average_sentiment_counts
-    colors = ['#00ff00', '#ff0000', '#ffff00']
-    plt.pie(sizes, labels=labels, colors=colors, autopct='%1.1f%%', startangle=90)
-    plt.axis('equal')
-    plt.title('Average Sentiment Analysis Results')
-    plt.show()
-
     html_response = "<h1>News Results</h1>"
+
     for result in results:
         html_response += f"<h2>{result['title']}</h2>"
         html_response += f"<p><strong>Sentiment:</strong> {result['sentiment']}</p>"
-        html_response += f"<p><strong>Positive Count:</strong> {result['positive_count']}</p>"
-        html_response += f"<p><strong>Negative Count:</strong> {result['negative_count']}</p>"
-        html_response += f"<p><strong>Neutral Count:</strong> {result['neutral_count']}</p>"
         html_response += f"<p><strong>Overall Polarity Score:</strong> {result['overall_polarity_score']}</p>"
         html_response += f"<p><a href='{result['url']}' target='_blank'>Read More</a></p>"
-        html_response += "<hr>"
+        html_response += "<hr/>"
 
+        # Analyze each article separately and create the pie chart for sentiment analysis
+        summarized_text = summarizer(parser.document, sentences_count=3)
+        sentiment_counts = [0, 0, 0]
+        for sentence in summarized_text:
+            sentiment_scores = analyzer.polarity_scores(str(sentence))
+            if sentiment_scores['compound'] >= 0.05:
+                sentiment_counts[0] += 1
+            elif sentiment_scores['compound'] <= -0.05:
+                sentiment_counts[1] += 1
+            else:
+                sentiment_counts[2] += 1
+
+    # Create the pie chart using sentiment counts
+    labels = ['Positive', 'Negative', 'Neutral']
+    sizes = sentiment_counts
+    colors = ['#00ff00', '#ff0000', '#ffff00']
+    plt.figure()
+    plt.pie(sizes, labels=labels, colors=colors, autopct='%1.1f%%', startangle=90)
+    plt.axis('equal')
+
+    # Sanitize the title to remove invalid characters from the filename
+    sanitized_title = re.sub(r'[^\w\s-]', '', result['title']).strip()
+    sanitized_title = re.sub(r'[-\s]+', '-', sanitized_title)
+
+    # Save the chart to a file
+    chart_filename = f'chart_{sanitized_title}.png'
+    plt.savefig(chart_filename)
+    plt.close()
+
+    # Encode the chart image as a base64 string
+    with open(chart_filename, 'rb') as image_file:
+        encoded_string = base64.b64encode(image_file.read()).decode('utf-8')
+
+    # Add the chart to the HTML response
+    html_response += "<p><strong>Sentiment Analysis Chart:</strong></p>"
+    html_response += f"<img src='data:image/png;base64,{encoded_string}' alt='Sentiment Analysis Chart'>"
+    html_response += "<hr/>"
+    
     return html_response
 
 if __name__ == '__main__':
     app.run(debug=True)
-
-
-    
